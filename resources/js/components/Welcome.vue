5bb1459643727cab908b1b5f28d5225eb492c66a
poboxx@lbteksystems.com <poboxx@lbteksystems.com>
2024-02-16 12:08:17 +0800

    deo commit

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Deo Docker laravel + postgresql</h1>
+  Hello {{ name }} welcome to my app.
+  <hr />
+  <counter></counter>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import Counter from "./Counter.vue";
+
+export default defineComponent({
+  components: {
+    Counter,
+  },
+  setup() {
+    const name = ref<string>("Everyone");
+    return {
+      name,
+    };
+  },
+});
+</script>
